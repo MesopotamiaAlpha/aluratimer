6a60766128a7011ed7cf9b2d6255ed3bee3bd704
José Antonio <leao-gomes@hotmail.com>
2022-07-08 11:52:03 -0300

    Iniciando módulo 4

---
+++ app/js/renderer.js
@@ -0,0 +1,30 @@
+const { ipcRenderer } = require("electron");
+const timer = require("./timer");
+
+let linkSobre = document.querySelector('#link-sobre');
+let botaoPlay = document.querySelector('.botao-play');
+let tempo = document.querySelector('.tempo');
+
+linkSobre.addEventListener('click', () => {
+    ipcRenderer.send('abrir-janela-sobre');
+});
+
+let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
+let play = false;
+botaoPlay.addEventListener('click', function () {
+        imgs = imgs.reverse();
+        
+        if (play) {
+            timer.parar();
+            
+            play = false;
+        }else{
+            timer.iniciar(tempo);
+            play = true;
+        }
+        
+        botaoPlay.src = imgs[0];
+});
+
+
+
--- app/js/sobre.js
+++ app/js/sobre.js
@@ -1,13 +1,18 @@
-const {ipcRenderer} = require('electron');
-const process = require('process');
+const { ipcRenderer, shell } = require('electron');
+    const process = require('process');
 
-let linkFechar  = document.querySelector('#link-fechar');
+let linkFechar = document.querySelector("#link-fechar");
+let linkTwitter = document.querySelector("#link-twitter");
 let versaoElectron = document.querySelector('#versao-electron');
 
-window.onload = function() {
+window.onload = function(){
     versaoElectron.textContent = process.versions.electron;
 }
 
-linkFechar.addEventListener('click', () => {
+linkFechar.addEventListener('click', function () {
     ipcRenderer.send('fechar-janela-sobre');
-});
+})
+
+linkTwitter.addEventListener('click', function () {
+    shell.openExternal("https://www.twitter.com/dquintanilhas");
+})
